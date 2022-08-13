cdb0ebb7cc7dec8a0c8d4038cba52f405f7e65ec
SKJKL <skamara766@gmail.com>
2022-08-13 20:42:50 +0100

    Push to GitHub

---
+++ html/home.php
@@ -0,0 +1,8 @@
+<h1>Welcome to MolData search</h1>
+<p>Type a chemical in the search box to get its safety data sheet.</p>
+<p class="small-tip">E.g. glucose, benzene, ethyl acetate, doxorubicin hydrochloride</p>
+
+<form action="search.php">
+	<input type="text" placeholder="Search.." name="molname" />
+	<input type="submit" />
+</form>
--- html/index.php
+++ html/index.php
@@ -1,5 +1,32 @@
-<?php
-	echo "Hello" . "\n";
-	echo "World!!";
-	echo var_dump($_SERVER);
-?>
+<!DOCTYPE html>
+<html lang="de" class="page-<?=str_replace("/", "-", $subView)?>">
+<head>
+	<meta charset="utf-8">
+	<title>MolData</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<link rel="stylesheet" type="text/css" href="master.css" media="screen">
+	<!-- <script charset="utf-8" type="text/javascript" src="js/master.js"></script> -->
+</head>
+<body>
+<div id="wrapper">
+	<header>
+		<div id="title">MolData Search</div>
+		<ul>
+			<li class="nav-home"><a href="/">Home</a></li>
+			<li class="nav-kontakt"><a href="/kontakt">Another tab</a></li>
+		</ul>
+	</header>
+	
+	<div id="content">
+		
+<?php require("home.php") ?>
+		
+	</div>
+	
+	<footer>&copy; Footer</footer>
+</div>
+</body>
+</html>
